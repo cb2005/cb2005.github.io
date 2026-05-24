e889c018810bb71401dd3f13eafb702cd8bf2c81
cb2005 <154026222+cb2005@users.noreply.github.com>
2026-05-24 17:23:49 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+const serviceEntry = document.getElementById("service-entry");
+const memoryGameEntry = document.getElementById("memory-game-entry");
+const ecommerceEntry = document.getElementById("ecommerce-entry");
+const analyticsEntry = document.getElementById("analytics-entry");
+
+serviceEntry.addEventListener("click", (e) => {
+  window.location.href ="service site/index.html";
+})
+memoryGameEntry.addEventListener("click", (e) => {
+  window.location.href = "memory game/index.html";
+})
+ecommerceEntry.addEventListener("click", (e) => {
+  window.location.href = "ecommerce site/index.html"
+})
+analyticsEntry.addEventListener("click", (e) => {
+  window.location.href = "analytics site/index.html"
+})
